31f5a14a48a101e8634bdeb0266c7f403e8775bd
Cédric Bélanger-St-Pierre <cedricbstpierre@yahoo.com>
2021-04-04 21:30:32 -0400

    Yup

--- Assessments Test.R
+++ Assessments Test.R
@@ -1,2 +1,16 @@
-library(tidyverse)
+p <- 5/38
+notP <- 1 - p
+a <- 6
+b <- -1
+n <- 500
 
+mu <- a*p+b*notP
+sigma <- abs(b-a) * sqrt(p*notP)
+
+mu*500
+
+sigma*sqrt(n)
+
+
+pnorm(0, mu*500, sigma*sqrt(500))
+d
